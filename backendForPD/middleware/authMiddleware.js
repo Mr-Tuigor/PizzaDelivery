a094d8909cc70072c1f3f3351da76e9150d284dd
Mr-Tuigor <shaharyaransarircb1@gmail.com>
2026-03-29 20:17:25 +0530

    Added adminOnly for adminOnly routes

--- backendForPD/middleware/authMiddleware.js
+++ backendForPD/middleware/authMiddleware.js
@@ -3,31 +3,62 @@ const userModel = require('../models/User');
 
 const protect = async (req, res, next) => {
     // console.log("In middleware");
-
+    try {
     let token;
 
     // Check if the 'token' cookie exists
     if (req.cookies.token) {
         // console.log("Got token");
-        try {
-
             token = req.cookies.token;
             const decoded = jwt.verify(token, process.env.JWT_SECRET); 
 
             req.user = await userModel.findById(decoded.id).select('-password');
             // console.log("heading to next");
             next(); 
-        } catch (error) {
-            console.error(error);
-            res.status(401).json({ message: 'Not authorized, token failed' });
-        }
     }
 
     if (!token) {
         console.log("no tokens found");
         res.status(401).json({ message: 'Not authorized, no token' });
     }
+
+    } catch (error) {
+            console.error(error);
+            res.status(401).json({ message: 'Not authorized, token failed' });
+        }
 };
 
+const adminOnly = async (req, res, next) => {
+    // console.log("In middleware");
+    try {
+    let token;
 
-module.exports = { protect };
+    // Check if the 'token' cookie exists
+    if (req.cookies.token) {
+        // console.log("Got token");
+
+            token = req.cookies.token;
+            const decoded = jwt.verify(token, process.env.JWT_SECRET); 
+
+            req.user = await userModel.findById(decoded.id).select('-password');
+            if(req.user.role === "admin"){
+                
+                next();
+            } else {
+                throw new Error("You must be admin to visit this page!");
+            }
+            // console.log("heading to next");
+    }
+
+    if (!token) {
+        console.log("no tokens found");
+        return res.status(401).json({ message: 'Not authorized, no token' });
+    }
+
+    } catch (error) {
+            console.error(error);
+            res.status(401).json({ message: 'Not authorized, token failed' });
+        }
+};
+
+module.exports = { protect, adminOnly };
