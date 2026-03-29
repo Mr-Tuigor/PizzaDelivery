4299b6c5967b27a1029a145d99276a4d661dcc75
Mr-Tuigor <shaharyaransarircb1@gmail.com>
2026-03-29 20:24:56 +0530

    Added the orders/all route to fetch orders for Admin panel dashboard

--- backendForPD/routes/orderRoutes.js
+++ backendForPD/routes/orderRoutes.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
-const { userOrders, verifyAndPlaceOrder, inventoryDetails, inventoryStatus, getUserOrders } = require('../controllers/orderController');
-const { protect } = require('../middleware/authMiddleware');
+const { userOrders, getAllOrders, verifyAndPlaceOrder, inventoryDetails, inventoryStatus, getUserOrders } = require('../controllers/orderController');
+const { protect, adminOnly } = require('../middleware/authMiddleware');
 
 router.post("/create", protect, verifyAndPlaceOrder);
 
 router.get("/my-orders", protect, userOrders);
+router.get("/all", adminOnly, getAllOrders);
 
 module.exports = router;
